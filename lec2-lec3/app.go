53195f51a78c5ae484b12b1cdd668155627b5049
tung <duytungbui9786@gmail.com>
2021-07-06 22:31:33 +0700

    xóa các file test

--- lec2-lec3/app.go
+++ lec2-lec3/app.go
@@ -33,33 +33,15 @@ const (
 )
 
 func main() {
+	// server.RunServer()
 	var a []int
 	a = readText("text.txt")
 	// fmt.Println(a)
-	// sorttext.FibonacciTo(100)
-	// fmt.Println(sorttext.BubbleSort("bigtosmall", a[]))
-	// fmt.Println(sorttext.BubbleSort("smalltobig", a))
+	sorttext.FibonacciTo(100)
+	fmt.Println(sorttext.BubbleSort("bigtosmall", a))
+	fmt.Println(sorttext.BubbleSort("smalltobig", a))
+	fmt.Println(sorttext.MergeSort(a))
 	fmt.Println(sorttext.QuickSort(a, 0, len(a)-1))
-	// server.RunServer()
+	sorttext.PrimeNumber(a)
 
-	// //crawl-data
-	// resp, err := http.Get("https://www.imdb.com/chart/top/?ref_=nv_mv_250")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// defer resp.Body.Close()
-	// fmt.Println("Response status:", resp.Status)
-	// scanner := bufio.NewScanner(resp.Body)
-	// for i := 0; scanner.Scan() && i < 2000; i++ {
-	// 	res := strings.Contains(scanner.Text(), "collection-detail__product-details text-align-left")
-	// 	if res == true {
-	// 		// fmt.Println(scanner.Text())
-	// 		for i := 0; scanner.Scan() && i < 5; i++ {
-	// 			fmt.Println(scanner.Text())
-	// 		}
-	// 	}
-	// }
-	// if err := scanner.Err(); err != nil {
-	// 	panic(err)
-	// }
 }
